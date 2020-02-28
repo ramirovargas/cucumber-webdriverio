c0dbaa9ed6c0c7b6c7937706792ee9d792568ea3
Ramiro Vargas <ramiro.vargas@masivian.com>
2020-02-28 17:17:08 -0500

    add login successfull

--- features/login.feature
+++ features/login.feature
@@ -1,7 +1,7 @@
 Feature: Login into losestudiantes
     As an user I want to authenticate myself within losestudiantes website in order to rate teachers
 
-Scenario Outline: Login failed with wrong inputs
+Scenario Outline: Login with wrong inputs
 
   Given I go to losestudiantes home screen
     When I open the login screen
@@ -13,3 +13,11 @@ Scenario Outline: Login failed with wrong inputs
       | email            | password | error                    |
       |                  |          | "Ingresa una contraseña" |
       | miso@gmail.com   |    1234  | "Upss! El correo y"      |
+
+Scenario: Login with  right inputs
+
+  Given I go to losestudiantes home screen
+    When I open the login screen
+    And I fill a right email and password
+    And I try to login
+    Then I expect to login sucessfull
--- features/step-definitions/index.js
+++ features/step-definitions/index.js
@@ -1,22 +1,22 @@
 //Complete siguiendo las instrucciones del taller
-var {Given} = require('cucumber');
-var {When} = require('cucumber');
-var {Then} = require('cucumber');
-var {expect} = require('chai');
+var { Given } = require('cucumber');
+var { When } = require('cucumber');
+var { Then } = require('cucumber');
+var { expect } = require('chai');
 
 Given('I go to losestudiantes home screen', () => {
   browser.url('/');
-  if($('button=Cerrar').isDisplayed()) {
+  if ($('button=Cerrar').isDisplayed()) {
     $('button=Cerrar').click();
   }
 });
 
 When('I open the login screen', () => {
-    const btnIngresar = $('button=Ingresar');
-    btnIngresar.waitForExist(8000);
-    btnIngresar.waitForDisplayed(8000);
-    browser.waitUntil(() => btnIngresar.isClickable());
-    btnIngresar.click();
+  const btnIngresar = $('button=Ingresar');
+  btnIngresar.waitForExist(5000);
+  btnIngresar.waitForDisplayed(5000);
+  browser.waitUntil(() => btnIngresar.isClickable());
+  btnIngresar.click();
 });
 
 When('I fill a wrong email and password', () => {
@@ -31,6 +31,21 @@ When('I fill a wrong email and password', () => {
   passwordInput.setValue('123467891');
 });
 
+When('I fill a right email and password', () => {
+  var cajaLogIn = $('.cajaLogIn');
+
+  var mailInput = cajaLogIn.$('input[name="correo"]');
+  mailInput.click();
+  mailInput.setValue('TestLogin@gmail.com');
+
+  var passwordInput = cajaLogIn.$('input[name="password"]');
+  passwordInput.click();
+  passwordInput.setValue('password123');
+});
+Then('I expect to login sucessfull', () => {
+    $('//*[@id="cuenta"]').waitForExist(15000);
+});
+
 When('I try to login', () => {
   var cajaLogIn = $('.cajaLogIn');
   cajaLogIn.$('button=Ingresar').click();
@@ -41,20 +56,23 @@ Then('I expect to not be able to login', () => {
 });
 
 
-When(/^I fill with (.*) and (.*)$/ , (email, password) => {
-    var cajaLogIn = $('.cajaLogIn');
-  
-   var mailInput = cajaLogIn.$('input[name="correo"]');
-   mailInput.click();
-   mailInput.keys(email);
-  
-   var passwordInput = cajaLogIn.$('input[name="password"]');
-   passwordInput.click();
-   passwordInput.keys(password)
-  });
-  
-  Then('I expect to see {string}', error => {
-   $('.aviso.alert.alert-danger').waitForDisplayed(8000);
-   var alertText = browser.$('.aviso.alert.alert-danger').getText();
-   expect(alertText).to.include(error);
-  });
+When(/^I fill with (.*) and (.*)$/, (email, password) => {
+  var cajaLogIn = $('.cajaLogIn');
+  cajaLogIn.waitForExist(5000);
+  cajaLogIn.waitForDisplayed(5000);
+  var mailInput = cajaLogIn.$('input[name="correo"]');
+  mailInput.waitForExist(5000);
+  mailInput.waitForDisplayed(5000);
+  mailInput.click();
+  mailInput.keys(email);
+
+  var passwordInput = cajaLogIn.$('input[name="password"]');
+  passwordInput.click();
+  passwordInput.keys(password)
+});
+
+Then('I expect to see {string}', error => {
+  $('.aviso.alert.alert-danger').waitForDisplayed(8000);
+  var alertText = browser.$('.aviso.alert.alert-danger').getText();
+  expect(alertText).to.include(error);
+});
